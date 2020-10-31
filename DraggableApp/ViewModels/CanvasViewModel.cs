ea0f56e6367f88b90327b101d4f0fc632a14876e
okadabasso <okadabasso@gmail.com>
2020-11-01 08:15:10 +0900

    オブジェクトのドラッグ移動を実装する

--- DraggableApp/ViewModels/CanvasViewModel.cs
+++ DraggableApp/ViewModels/CanvasViewModel.cs
@@ -13,30 +13,48 @@ namespace DraggableApp.ViewModels
     public class CanvasViewModel : BindableBase
     {
         public ReactiveProperty<string> SampleText { get; set; } = new ReactiveProperty<string>("sample");
-        public DelegateCommand<DragDeltaEventArgs> DragDeltaCommand { get; set; }
-        public DelegateCommand<DragStartedEventArgs> DragStartedCommand { get; set; }
+        public DelegateCommand<DragDeltaEventArgs> ResizableDragDeltaCommand { get; set; }
+        public DelegateCommand<DragStartedEventArgs> ResizableDragStartedCommand { get; set; }
+        public DelegateCommand<DragDeltaEventArgs> DraggableDragDeltaCommand { get; set; }
+        public DelegateCommand<DragStartedEventArgs> DraggableDragStartedCommand { get; set; }
 
         public ReactiveProperty<double> X { get; set; } = new ReactiveProperty<double>();
         public ReactiveProperty<double> Y { get; set; } = new ReactiveProperty<double>();
 
-        public ReactiveProperty<int> Width { get; set; } = new ReactiveProperty<int>();
-        public ReactiveProperty<int> Height { get; set; } = new ReactiveProperty<int>();
+        public ReactiveProperty<int> ResizableWidth { get; set; } = new ReactiveProperty<int>();
+        public ReactiveProperty<int> ResizableHeight { get; set; } = new ReactiveProperty<int>();
+        public ReactiveProperty<int> DraggableTop { get; set; } = new ReactiveProperty<int>(200);
+        public ReactiveProperty<int> DraggableLeft { get; set; } = new ReactiveProperty<int>(100);
         private Point InitialPosition;
 
         public CanvasViewModel()
         {
-            DragDeltaCommand = new DelegateCommand<DragDeltaEventArgs>((x) =>
+            ResizableDragDeltaCommand = new DelegateCommand<DragDeltaEventArgs>((x) =>
             {
                 X.Value = x.HorizontalChange;
                 Y.Value = x.VerticalChange;
 
-                Width.Value = (int) InitialPosition.X + (int) x.HorizontalChange;
-                Height.Value = (int)InitialPosition.Y + (int)x.VerticalChange;
-                InitialPosition = new Point { X = Width.Value, Y = Height.Value };
+                ResizableWidth.Value = (int) InitialPosition.X + (int) x.HorizontalChange;
+                ResizableHeight.Value = (int)InitialPosition.Y + (int)x.VerticalChange;
+                InitialPosition = new Point { X = ResizableWidth.Value, Y = ResizableHeight.Value };
             });
-            DragStartedCommand = new DelegateCommand<DragStartedEventArgs>((x) =>
+            ResizableDragStartedCommand = new DelegateCommand<DragStartedEventArgs>((x) =>
             {
-                InitialPosition = new Point { X = Width.Value, Y = Height.Value };
+                InitialPosition = new Point { X = ResizableWidth.Value, Y = ResizableHeight.Value };
+            });
+
+            DraggableDragDeltaCommand = new DelegateCommand<DragDeltaEventArgs>((x) =>
+            {
+                X.Value = x.HorizontalChange;
+                Y.Value = x.VerticalChange;
+
+                DraggableLeft.Value += (int)x.HorizontalChange;
+                DraggableTop.Value += (int)x.VerticalChange;
+                InitialPosition = new Point { X = DraggableTop.Value, Y = DraggableLeft.Value };
+            });
+            DraggableDragStartedCommand = new DelegateCommand<DragStartedEventArgs>((x) =>
+            {
+                InitialPosition = new Point { X = DraggableTop.Value, Y = DraggableLeft.Value };
             });
 
         }
