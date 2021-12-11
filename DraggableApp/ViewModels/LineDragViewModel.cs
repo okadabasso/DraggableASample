a01c88ab5fda4d34e21bafb58bcfea48ddd13a4e
okadabasso <okadabasso@gmail.com>
2021-12-11 17:09:42 +0900

    線の移動時にカーソルを変更する

--- DraggableApp/ViewModels/LineDragViewModel.cs
+++ DraggableApp/ViewModels/LineDragViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
 
@@ -37,6 +38,7 @@ namespace DraggableApp.ViewModels
             new LineGeometry(new Point(100,200), new Point(200,200)),
         };
 
+ 
         int lineIndex;
         public ReactiveProperty<string> Message { get; set; } = new ReactiveProperty<string>();
         public LineDragViewModel()
@@ -56,138 +58,9 @@ namespace DraggableApp.ViewModels
             });
 
             // line drag
-            LineDragDeltaCommand = new DelegateCommand<DragDeltaEventArgs>((args) =>
-            {
-                var target = Lines[lineIndex];
-                if(lineIndex == 0)
-                {
-                    Lines.Insert(0, new LineGeometry(target.StartPoint, target.EndPoint));
-                    Points.Insert(0, new Point(target.StartPoint.X, target.StartPoint.Y));
-                    lineIndex = 1;
-                }
-                else if((lineIndex +1) >= Lines.Count)
-                {
-                    Lines.Add(new LineGeometry(target.StartPoint, target.EndPoint));
-                    Points.Add(new Point(target.EndPoint.X, target.EndPoint.Y));
-                }
-
-                if (target.StartPoint.X == target.EndPoint.X)
-                {
-                    var positionX = InitialPosition.X + (int)args.HorizontalChange; // horizontal
-                    target.StartPoint = new Point(positionX, target.StartPoint.Y);
-                    target.EndPoint = new Point(positionX, target.EndPoint.Y);
-
-                    Lines[lineIndex - 1].EndPoint = new Point(target.StartPoint.X, target.StartPoint.Y);
-                    Lines[lineIndex + 1].StartPoint= new Point(target.EndPoint.X, target.EndPoint.Y);
-
-                    Points[lineIndex] = new Point(target.StartPoint.X, target.StartPoint.Y);
-                    Points[lineIndex + 1] = new Point(target.EndPoint.X, target.EndPoint.Y);
-
-
-                }
-                if (target.StartPoint.Y == target.EndPoint.Y)
-                {
-                    var positionY = InitialPosition.Y + (int)args.VerticalChange; // vertical
-                    target.StartPoint = new Point(target.StartPoint.X, positionY);
-                    target.EndPoint = new Point(target.EndPoint.X, positionY);
-
-                    Lines[lineIndex - 1].EndPoint = new Point(target.StartPoint.X, target.StartPoint.Y);
-                    Lines[lineIndex + 1].StartPoint = new Point(target.EndPoint.X, target.EndPoint.Y);
-
-                    Points[lineIndex] = new Point(target.StartPoint.X, target.StartPoint.Y);
-                    Points[lineIndex + 1] = new Point(target.EndPoint.X, target.EndPoint.Y);
-
-                }
-
-
-                X.Value = args.HorizontalChange;
-                Y.Value = target.StartPoint.Y;
-                args.Handled = true;
-                RaisePropertyChanged(nameof(Points));
-            });
-            LineDragCompletedCommand = new DelegateCommand<DragCompletedEventArgs>((args) =>
-            {
-                if (Lines[lineIndex + 1].StartPoint == Lines[lineIndex + 1].EndPoint)
-                {
-                    Lines.Remove(Lines[lineIndex + 1]);
-                    if ((lineIndex + 1) < Lines.Count)
-                    {
-                        if (Lines[lineIndex].StartPoint.X == Lines[lineIndex + 1].StartPoint.X ||
-                        Lines[lineIndex].StartPoint.Y == Lines[lineIndex + 1].StartPoint.Y)
-                        {
-                            Lines[lineIndex].EndPoint = Lines[lineIndex + 1].EndPoint;
-                            Lines.Remove(Lines[lineIndex + 1]);
-                        }
-                    }
-                }
-                if(lineIndex > 0)
-                {
-                    if (Lines[lineIndex - 1].StartPoint == Lines[lineIndex - 1].EndPoint)
-                    {
-                        Lines.Remove(Lines[lineIndex - 1]);
-                        if ((lineIndex - 2) >= 0)
-                        {
-                            if (Lines[lineIndex - 2].StartPoint.X == Lines[lineIndex - 1].StartPoint.X ||
-                            Lines[lineIndex - 2].StartPoint.Y == Lines[lineIndex - 1].StartPoint.Y)
-                            {
-                                Lines[lineIndex - 1].StartPoint = Lines[lineIndex - 2].StartPoint;
-                                Lines.Remove(Lines[lineIndex - 2]);
-                            }
-                        }
-                    }
-
-                }
-                if (Points[lineIndex + 1] == Points[lineIndex + 2])
-                {
-                    Points.Remove(Points[lineIndex + 1]);
-                    if ((lineIndex + 2) < Points.Count)
-                    {
-                        if (Points[lineIndex].X == Points[lineIndex + 1].X && Points[lineIndex + 1].X == Points[lineIndex + 2].X ||
-                        Points[lineIndex].Y == Points[lineIndex + 1].Y && Points[lineIndex + 1].Y == Points[lineIndex + 2].Y)
-                        {
-                            Points.Remove(Points[lineIndex + 1]);
-                        }
-                    }
-                }
-                if(lineIndex > 0)
-                {
-                    if (Points[lineIndex - 1] == Points[lineIndex])
-                    {
-                        Points.Remove(Points[lineIndex]);
-                        lineIndex--;
-                        if ((lineIndex - 1) >= 0)
-                        {
-                            if (Points[lineIndex - 1].X == Points[lineIndex].X ||
-                            Points[lineIndex - 1].Y == Points[lineIndex].Y)
-                            {
-                                Points.Remove(Points[lineIndex]);
-                            }
-                        }
-                    }
-
-                }
-                RaisePropertyChanged(nameof(Points));
-
-                var message = "";
-                foreach(var point in Points)
-                {
-                    message += point.ToString() + Environment.NewLine;
-                }
-                message +=  Environment.NewLine;
-                foreach (var line in Lines)
-                {
-                    message += line.StartPoint.ToString() + " " + line.EndPoint.ToString() + Environment.NewLine;
-                }
-                Message.Value = message;
-
-            });
-            LineDragStartedCommand = new DelegateCommand<DragStartedEventArgs>((x) =>
-            {
-                
-                var source = (x.Source as Thumb).DataContext as LineGeometry ;
-                lineIndex = Lines.Select((v,i) => new { line = v, i }).Where(x => x.line == source).Select(x => x.i).First();
-                InitialPosition = new Point { X = source.StartPoint.X, Y = source.StartPoint.Y };
-            });
+            LineDragDeltaCommand = new DelegateCommand<DragDeltaEventArgs>(OnDragDelta);
+            LineDragCompletedCommand = new DelegateCommand<DragCompletedEventArgs>(OnDragCompleted);
+            LineDragStartedCommand = new DelegateCommand<DragStartedEventArgs>(OnDragStarted);
 
         }
         public void DoEvents()
@@ -200,6 +73,140 @@ namespace DraggableApp.ViewModels
             });
             Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, callback, frame);
             Dispatcher.PushFrame(frame);
+        }
+        private void OnDragStarted(DragStartedEventArgs args)
+        {
+            var source = (args.Source as Thumb).DataContext as LineGeometry;
+            lineIndex = Lines.Select((v, i) => new { line = v, i })
+                .Where(x => x.line == source)
+                .Select(x => x.i).First();
+            InitialPosition = new Point { X = source.StartPoint.X, Y = source.StartPoint.Y };
+
+        }
+        private void OnDragDelta(DragDeltaEventArgs args)
+        {
+            var target = Lines[lineIndex];
+            if (lineIndex == 0)
+            {
+                Lines.Insert(0, new LineGeometry(target.StartPoint, target.EndPoint));
+                Points.Insert(0, new Point(target.StartPoint.X, target.StartPoint.Y));
+                lineIndex = 1;
+            }
+            else if ((lineIndex + 1) >= Lines.Count)
+            {
+                Lines.Add(new LineGeometry(target.StartPoint, target.EndPoint));
+                Points.Add(new Point(target.EndPoint.X, target.EndPoint.Y));
+            }
+
+            if (target.StartPoint.X == target.EndPoint.X)
+            {
+                var positionX = InitialPosition.X + (int)args.HorizontalChange; // horizontal
+                target.StartPoint = new Point(positionX, target.StartPoint.Y);
+                target.EndPoint = new Point(positionX, target.EndPoint.Y);
+
+                Lines[lineIndex - 1].EndPoint = new Point(target.StartPoint.X, target.StartPoint.Y);
+                Lines[lineIndex + 1].StartPoint = new Point(target.EndPoint.X, target.EndPoint.Y);
+
+                Points[lineIndex] = new Point(target.StartPoint.X, target.StartPoint.Y);
+                Points[lineIndex + 1] = new Point(target.EndPoint.X, target.EndPoint.Y);
+
+
+            }
+            if (target.StartPoint.Y == target.EndPoint.Y)
+            {
+                var positionY = InitialPosition.Y + (int)args.VerticalChange; // vertical
+                target.StartPoint = new Point(target.StartPoint.X, positionY);
+                target.EndPoint = new Point(target.EndPoint.X, positionY);
+
+                Lines[lineIndex - 1].EndPoint = new Point(target.StartPoint.X, target.StartPoint.Y);
+                Lines[lineIndex + 1].StartPoint = new Point(target.EndPoint.X, target.EndPoint.Y);
+
+                Points[lineIndex] = new Point(target.StartPoint.X, target.StartPoint.Y);
+                Points[lineIndex + 1] = new Point(target.EndPoint.X, target.EndPoint.Y);
+
+            }
+
+
+            X.Value = args.HorizontalChange;
+            Y.Value = target.StartPoint.Y;
+            args.Handled = true;
+            RaisePropertyChanged(nameof(Points));
+
+        }
+        private void OnDragCompleted(DragCompletedEventArgs args)
+        {
+            if (Lines[lineIndex + 1].StartPoint == Lines[lineIndex + 1].EndPoint)
+            {
+                Lines.Remove(Lines[lineIndex + 1]);
+                if ((lineIndex + 1) < Lines.Count)
+                {
+                    if (Lines[lineIndex].StartPoint.X == Lines[lineIndex + 1].StartPoint.X ||
+                    Lines[lineIndex].StartPoint.Y == Lines[lineIndex + 1].StartPoint.Y)
+                    {
+                        Lines[lineIndex].EndPoint = Lines[lineIndex + 1].EndPoint;
+                        Lines.Remove(Lines[lineIndex + 1]);
+                    }
+                }
+            }
+            if (lineIndex > 0)
+            {
+                if (Lines[lineIndex - 1].StartPoint == Lines[lineIndex - 1].EndPoint)
+                {
+                    Lines.Remove(Lines[lineIndex - 1]);
+                    if ((lineIndex - 2) >= 0)
+                    {
+                        if (Lines[lineIndex - 2].StartPoint.X == Lines[lineIndex - 1].StartPoint.X ||
+                        Lines[lineIndex - 2].StartPoint.Y == Lines[lineIndex - 1].StartPoint.Y)
+                        {
+                            Lines[lineIndex - 1].StartPoint = Lines[lineIndex - 2].StartPoint;
+                            Lines.Remove(Lines[lineIndex - 2]);
+                        }
+                    }
+                }
+
+            }
+            if (Points[lineIndex + 1] == Points[lineIndex + 2])
+            {
+                Points.Remove(Points[lineIndex + 1]);
+                if ((lineIndex + 2) < Points.Count)
+                {
+                    if (Points[lineIndex].X == Points[lineIndex + 1].X && Points[lineIndex + 1].X == Points[lineIndex + 2].X ||
+                    Points[lineIndex].Y == Points[lineIndex + 1].Y && Points[lineIndex + 1].Y == Points[lineIndex + 2].Y)
+                    {
+                        Points.Remove(Points[lineIndex + 1]);
+                    }
+                }
+            }
+            if (lineIndex > 0)
+            {
+                if (Points[lineIndex - 1] == Points[lineIndex])
+                {
+                    Points.Remove(Points[lineIndex]);
+                    lineIndex--;
+                    if ((lineIndex - 1) >= 0)
+                    {
+                        if (Points[lineIndex - 1].X == Points[lineIndex].X ||
+                        Points[lineIndex - 1].Y == Points[lineIndex].Y)
+                        {
+                            Points.Remove(Points[lineIndex]);
+                        }
+                    }
+                }
+
+            }
+            RaisePropertyChanged(nameof(Points));
+
+            var message = "";
+            foreach (var point in Points)
+            {
+                message += point.ToString() + Environment.NewLine;
+            }
+            message += Environment.NewLine;
+            foreach (var line in Lines)
+            {
+                message += line.StartPoint.ToString() + " " + line.EndPoint.ToString() + Environment.NewLine;
+            }
+            Message.Value = message;
         }
     }
 }
--- DraggableApp/Views/LineDragView.xaml.cs
+++ DraggableApp/Views/LineDragView.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows.Controls;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Input;
 
 namespace DraggableApp.Views
 {
@@ -13,5 +16,29 @@ namespace DraggableApp.Views
             InitializeComponent();
         }
 
+        private void Thumb_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+        }
+
+        private void Thumb_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var line = (e.Source as Thumb).DataContext as LineGeometry;
+            if(line.StartPoint.X == line.EndPoint.X)
+            {
+                Cursor = Cursors.SizeWE;
+            }
+            if (line.StartPoint.Y == line.EndPoint.Y)
+            {
+                Cursor = Cursors.SizeNS;
+            }
+
+        }
+
+        private void Thumb_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Cursor = Cursors.Arrow;
+
+        }
     }
 }
